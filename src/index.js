3ee99799ac0a4b5526ac5e0dae0effdf17aa2962
Rodrigo Bossini <professorbossini@gmail.com>
2024-02-23 18:06:59 -0300

    feat(props): exibe pedidos sem componentes

---
+++ src/index.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+const App = () => {
+  return (
+    <div className="container border rounded mt-2">
+      
+      <div className="row">
+        <div className="col-12">
+          <h1 className="display-5 text-center">Seus pedidos</h1>  
+        </div>
+      </div>
+
+      <div className="row">
+
+        <div className="col-sm-12 col-lg-4">
+          <div className="card">
+            <div className="card-header text-muted">
+              20/04/2023
+            </div>
+            <div className="card-body d-flex">
+              <div className='d-flex align-items-center'>
+                <i className="fa-solid fa-book fa-2x"></i>
+              </div>
+              <div className='flex-grow-1 ms-2 border p-2'>
+                <h4 className="text-center">Livro</h4>
+                <p className='text-center'>Concrete Mathematics - Donald Knuth</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-12 col-lg-4">
+          <div className="card">
+              <div className="card-header text-muted">
+                21/04/2023
+              </div>
+              <div className="card-body d-flex">
+                <div className='d-flex align-items-center'>
+                  <i className="fa-solid fa-laptop fa-2x"></i>
+                </div>
+                <div className='flex-grow-1 ms-2 border p-2'>
+                  <h4 className="text-center">Notebook</h4>
+                  <p className='text-center'>i7 - 16Gb</p>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <div className="col-sm-12 col-lg-4">
+          <div className="card">
+              <div className="card-header text-muted">
+                21/02/2024
+              </div>
+              <div className="card-body d-flex">
+                <div className='d-flex align-items-center'>
+                  <i className="fa-solid fa-memory fa-2x"></i>
+                </div>
+                <div className='flex-grow-1 ms-2 border p-2'>
+                  <h4 className="text-center">SSD</h4>
+                  <p className='text-center'>256Gb</p>
+                </div>
+              </div>
+            </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App/>,
+  document.querySelector('#root')
+)
