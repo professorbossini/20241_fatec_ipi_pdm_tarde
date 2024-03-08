a190dbb1d74f1d41a44ff8a3daff1b35fb0ef3b7
Rodrigo Bossini <professorbossini@gmail.com>
2024-03-08 17:58:00 -0300

    feat(ap05): fim da secao 2.9
    - implementa funcao de obtencao de localizacao que atualiza o estado
    - implementa a JSX, com alguns detalhes faltando
    - lida com potenciais erros de maneira simplificada e diferente da aposila

--- estacao-climatica/src/index.js
+++ estacao-climatica/src/index.js
@@ -11,7 +11,8 @@ class App extends React.Component{
       longitude: null,
       estacao: null,
       data: null,
-      icone: null
+      icone: null,
+      erro: null
     }
   }
   obterEstacao = (data, latitude) => {
@@ -34,15 +35,87 @@ class App extends React.Component{
     return estouNoSul ? 'Outono' : 'Primavera'
   }
 
+  obterLocalizacao = () => {
+    //usando a api de geolocalizacao, extrair a localizacao do usuário
+    window.navigator.geolocation.getCurrentPosition(
+      (posicao) => {
+          //quando estiver disponível:
+          // extrair a data atual do sistema
+          let dataAtual = new Date()
+          //encontrar a estacao climatica usando a data e o local
+          let estacao = this.obterEstacao(dataAtual, posicao.coords.latitude)
+          //acessar o mapa de icones e pegar aquele que esta associado a estacao detectada
+          let icone = this.icones[estacao]
+          //atualizar o estado
+          this.setState({
+            latitude: posicao.coords.latitude,
+            longitude: posicao.coords.longitude,
+            estacao: estacao,
+            data: dataAtual.toLocaleTimeString(),
+            icone: icone
+          })
+
+      },
+      (erro) => {
+        console.log(erro)
+        this.setState({
+          erro: 'Por favor, permita o acesso'
+        })
+        //faça com que o texto "por favor, permita o acesso" apareça na tela
+      }
+    )
+  }
+
   icones = {
     'Primavera': 'fa-seedling',
     'Verão': 'fa-sun',
     'Outono': 'fa-canadian-maple-leaf',
     'Inverno': 'fa-snowflake'
   }
+  
   render(){
     return(
-      <div>Meu app</div>
+      <div className="container mt-4">
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-8">
+              <div className="card">
+                {/* corpo */}
+                <div className="card-body">
+
+                  <div
+                    style={{height: '6rem'}} 
+                    className="ps-2 d-flex align-items-center border rounded mb-2">
+                      <i className={`fa-solid fa-5x ${this.state.icone}`}></i>
+                      <p className="w-75 ms-3 text-center fs-1">{this.state.estacao}</p>
+                  </div>
+
+                  <div>
+                    
+                    <p className="text-center mt-3">
+                      {
+                        this.state.latitude ? 
+                        `Coords: ${this.state.latitude},${this.state.longitude}. Data: ${this.state.data}` : 
+                        `Clique no botão para saber a sua estação climática`
+                      }
+                    </p>
+                    <p className="text-center text-danger">
+                      {this.state.erro}
+                    </p>
+
+                  </div>
+                  <div>
+                    <button 
+                      className="btn btn-outline-primary w-100 mt-2"
+                      onClick={this.obterLocalizacao}>
+                      Qual a minha estação?
+                    </button>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
     )
   }
 }
