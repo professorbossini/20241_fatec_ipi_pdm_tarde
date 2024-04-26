ebaebf8b97c683855b8463471011ebba11bda9f0
Rodrigo Bossini <professorbossini@gmail.com>
2024-04-26 16:11:22 -0300

    feat(ap07): encerra a secao 2.12
    - exibe o logo da Pexels

--- busca-fotos/src/components/App.js
+++ busca-fotos/src/components/App.js
@@ -3,6 +3,7 @@ import Busca from "./Busca";
 import env from 'react-dotenv'
 import { createClient } from 'pexels'
 import ListaImagens from './ListaImagens';
+import PexelsLogo from './PexelsLogo';
 
 export default class App extends React.Component{
 
@@ -13,7 +14,7 @@ export default class App extends React.Component{
   pexelsClient = null
   onBuscaRealizada = (termo) => {
     this.pexelsClient.photos.search({query: termo, per_page: 2}).then((result) => {
-      // console.log(photos)
+      console.log(result.photos)
       this.setState({photos: result.photos})
 
     })
@@ -28,6 +29,9 @@ export default class App extends React.Component{
       <div 
         className="grid border-round border-1 border-400 justify-content-center w-9 m-auto">
           <div className="col-12">
+            <PexelsLogo />
+          </div>
+          <div className="col-12">
             <h1>Exibir uma lista de...</h1>
           </div>
           <div className="col-8">
@@ -35,9 +39,7 @@ export default class App extends React.Component{
               onBuscaRealizada={this.onBuscaRealizada}/>
           </div>
           <div className="col-8">
-            {
-              <ListaImagens photos={this.state.photos}/>
-            }
+            <ListaImagens photos={this.state.photos}/>
           </div>
       </div>
     )
