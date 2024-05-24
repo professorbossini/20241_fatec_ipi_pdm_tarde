02726cb75d468925dbbf09a02f5ba9abf5730d7b
Rodrigo Bossini <professorbossini@gmail.com>
2024-05-24 17:22:39 -0300

    feat(ap002-rn): encerra a apostila
    - vamos retomar fazendo um Back End

--- react_native/lembretes/App.tsx
+++ react_native/lembretes/App.tsx
@@ -17,80 +17,66 @@ import {
 } from '@expo/vector-icons';
 
 type Lembrete = {
-  id: string;
+  id?: string;
   texto: string;
 }
 
 export default function App() {
-  const [lembrete, setLembrete] = useState <string>('')
+  const [lembrete, setLembrete] = useState <Lembrete | null>(null)
   const [lembretes, setLembretes] = useState <Lembrete[]> ([])
+  const [emModoEdicao, setEmModoEdicao] = useState(false)
   const adicionar = () => {
-    if(lembrete === '')
+    if(lembrete?.texto === '')
         alert('Lembrete não pode ser vazio')
     else{
       const novoLembrete: Lembrete = {
         id: Date.now().toString(),
-        texto: lembrete
+        texto: lembrete!.texto
       }
   
       setLembretes( lembretesAtual => [
         novoLembrete,
         ...lembretesAtual
       ])
-      setLembrete('')
+      setLembrete({texto: ''})
     }
   }
 
   const remover: Function = (lembrete: Lembrete): void => {
-    //exibe um Alert para confirmar se o usuário quer mesmo remover
-    Alert.alert(
-      //titulo
-      'Remover lembrete',
-      //mensagem
-      `Deseja remover este lembrete? ${lembrete.texto}`,
-      //coleção de botões
-      [
-        {
-          text: 'Cancelar',
-          style: 'cancel'
-        },
-        {
-          text: 'Remover',
-          style: 'destructive',
-          onPress: () => {
-            setLembretes(
-              lembretesAtual => lembretesAtual.filter(item => item.id !== lembrete.id)
-            )
-          }
-        }
-      ]
-    )
-    //busca na lista pelo id do lembrete
-    //remove da lista
-    //atualiza o estado
+    setLembretes(lembretesAtual => lembretesAtual.filter(l => l.id !== lembrete.id))
   }
 
   const atualizar = () => {
-    
+    console.log('atualizar')
+    console.log(lembrete)
+    //1. Atualizar a lista, para que o novo texto seja exibido (é preciso encontrar o lembrete de id igual ao daquele que foi atualizado)
+    const aux: Lembrete[] = lembretes.map(l => l.id === lembrete!.id ? lembrete! : l)
+    console.log(aux)
+    setLembretes(aux)
+    //2. Limpar o campo textual
+    setLembrete({texto: ''})
+    //3. Troca o modo da aplicação, ela deixa de estar em modo de atualização e volta ao modo de cadastro
+    setEmModoEdicao(false)
   }
   return (
     <View style={styles.container}>
       <TextInput
         style={styles.input} 
         placeholder='Digite um lembrete...'
-        value={lembrete}
-        onChangeText={setLembrete}
+        value={lembrete?.texto}
+        onChangeText={(lembreteDigitado) => setLembrete({id: lembrete?.id, texto: lembreteDigitado })}
       />
       <Pressable
         style={styles.button}
-        onPress={adicionar}>
+        onPress={emModoEdicao ? atualizar : adicionar}>
         <Text
           style={styles.buttonText}>
-          Salvar lembrete
+          {/* Salvar lembrete ou Atualizar lembrete*/}
+          {`${emModoEdicao ? 'Atualizar' : 'Adicionar'} lembrete`}
         </Text>
       </Pressable>
       <FlatList
-        keyExtractor={item => item.id}
+        keyExtractor={item => item.id!}
         style={styles.list} 
         data={lembretes}
         renderItem={lembrete => (
@@ -109,7 +95,12 @@ export default function App() {
                   size={24}>
                 </AntDesign>
               </Pressable>
-              <Pressable>
+              <Pressable
+                onPress={() => {
+                    console.log(lembrete.item.id)
+                    setLembrete(lembrete.item)
+                    setEmModoEdicao(true)
+                }}>
                 <AntDesign 
                   name='edit'
                   size={24}
@@ -164,11 +155,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#F0F0F0',
     textAlign: 'center',
     margin: 8,
-    flexDirection: 'row'
+    flexDirection: 'row',
+    alignItems: 'center'
   },
   listItemText:{
     width: '70%',
-    textAlign: 'center'
+    textAlign: 'center',
+    // alignSelf: 'center'
   },
   listItemButtons: {
     flexDirection: 'row',
